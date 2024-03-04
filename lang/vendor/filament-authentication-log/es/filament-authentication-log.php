7b2918146cfd6c04503b4792f16a7ad387035923
Pablo Muñoz <pablomunoz@besoccer.com>
2024-03-04 17:46:26 +0100

    feat: logs logins

---
+++ lang/vendor/filament-authentication-log/es/filament-authentication-log.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'navigation.group' => 'Herramientas',
+
+    'navigation.authentication-log.label' => 'Registro de autenticación',
+    'navigation.authentication-log.plural-label' => 'Registros de autenticación',
+
+    'table.heading' => 'Registros de autenticación',
+
+    'column.authenticatable' => 'authenticatable_id',
+    'column.ip_address' => 'Dirección IP',
+    'column.user_agent' => 'Agente de usuario',
+    'column.login_at' => 'Inicio de sesión en',
+    'column.login_successful' => 'Inicio de sesión exitoso',
+    'column.logout_at' => 'Cierre de sesión en',
+    'column.cleared_by_user' => 'Borrado por el usuario',
+];
